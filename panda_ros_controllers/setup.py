2209037deaf6872745504ea92c17f3d97b33828d
jun yamada <junyamada107@gmail.com>
2022-10-19 14:10:45 +0000

    init

---
+++ panda_ros_controllers/setup.py
@@ -0,0 +1,13 @@
+## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
+
+import distutils.core 
+from catkin_pkg.python_setup import generate_distutils_setup
+
+# fetch values from package.xml
+setup_args = generate_distutils_setup(
+    packages=['panda_ros_controllers'],
+    package_dir={'': 'src'},
+    requires=['std_msgs', 'rospy', 'message_filters', 'sensor_msgs']
+)
+
+distutils.core.setup(**setup_args)
